e0227df6fc719ea45da6d5063ed9ac0f1651ffe6
Hyun <ikimyh059554@gmail.com>
2025-10-05 15:52:39 +0900

    feat: Tab 이미지 추가 및 기능 컨테이너 개발

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,44 +1,13 @@
-import { Mission } from "@/components/mission";
-import { StyleSheet, Text, View } from "react-native";
+import { router } from "expo-router";
+import { Pressable, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    // <SafeAreaView>
-    //   <View style={styles.parent}>
-    //     <View style={styles.container}>
-    //       <Text style={styles.text}>텍스트</Text>
-    //     </View>
-    //     {/* <TextInput value={text} onChangeText={(value) => setText(value)} style={styles.input} />
-    //   <Button title="버튼이름" onPress={() => console.log("pressed!")} /> */}
-    //     <View style={styles.container2}>
-    //       <Text style={styles.text}>Home</Text>
-    //       <Text style={styles.text}>Home</Text>
-    //       <Text style={styles.text}>Home</Text>
-    //     </View>
-    //   </View>
-    // </SafeAreaView>
-    <Mission />
+    <SafeAreaView>
+      <Pressable onPress={() => router.push("/my")}>
+        <Text>홈 스크린</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  parent: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  container: {
-    backgroundColor: "yellow",
-  },
-  container2: {
-    backgroundColor: "blue",
-  },
-  text: {
-    color: "red",
-    fontSize: 30,
-  },
-  input: {
-    fontSize: 30,
-  },
-});
