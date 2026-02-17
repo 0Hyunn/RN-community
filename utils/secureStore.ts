7f7c565775503617403d57b4db45f88bf7e0e61c
Hyun <ikimyh059554@gmail.com>
2026-02-17 20:36:10 +0900

    auth: 인증 관련 코드 추가

--- utils/secureStore.ts
+++ utils/secureStore.ts
@@ -1,15 +1,18 @@
 import * as SecureStore from "expo-secure-store";
 
+// expo-secure-store
 async function saveSecureStore(key: string, value: string) {
   await SecureStore.setItemAsync(key, value);
 }
 
+// auth.ts 에서 인증 시 사용
 async function getSecureStore(key: string) {
   const storedData = (await SecureStore.getItemAsync(key)) ?? null;
 
   return storedData;
 }
 
+// 삭제 시
 async function deleteSecureStore(key: string) {
   await SecureStore.deleteItemAsync(key);
 }
